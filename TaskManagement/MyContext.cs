def9be7d8656b3e315464637f756a6de48277d00
Thilan Dissanayaka <43875064+Thil4n@users.noreply.github.com>
2023-02-28 07:54:44 +0530

    init

---
+++ TaskManagement/MyContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
+
+
+public class MyContext : DbContext
+{
+    public DbSet<Task> MyEntities { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseMySql("server=127.0.0.1;port=3306;uid=thilan;pwd=p455w07d;database=taskmanagement", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.31-mysql"));
+        }
+    }
+}
+
